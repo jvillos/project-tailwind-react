6bcd78a58840d4bb32e99cf2a1fbf20a45818b7b
DanielSanrui <daniel.sanc.ruiz@gmail.com>
2025-02-17 13:57:05 +0100

    tailwind funciona

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ interface HeaderProps {
 function Header({ logoSource }: HeaderProps) {
   return (
     <div className="flex justify-between items-center p-4 bg-gray-900 text-white">
-      <img src={logoSource} alt="logo" />
+      <img src={logoSource} alt="logo" width="200" />
       <nav className="hidden md:flex space-x-6 ms-4 me-4">
         <a href="#" className="hover:underline">Nosotros</a>
         <a href="#" className="hover:underline">Servicios</a>
